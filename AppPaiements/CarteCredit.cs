ce8ba36890203bb423b593fb9d2b1b30bcb2cedf
Souleymane Diallo <souleymane.diallo2@monboreal.ca>
2024-10-18 17:31:17 -0400

    Modification de la classe CarteCredit reussit

--- AppPaiements/CarteCredit.cs
+++ AppPaiements/CarteCredit.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace AppPaiements
 {
-    internal class CarteCredit
+    internal class CarteCredit : Paiement 
     {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int numerocarte):base(montant, description) 
+        {
+            NumeroCarte = numerocarte;
+        }
+
+
+        public override void AfficherDetails() 
+        {
+            Console.WriteLine($"Montant : {Montant}, description : {Description}, numerocarte : {NumeroCarte}");
+        }
+
 
     }
 }
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit achat1 = new CarteCredit(300, "Chemise", 12345);
+        CarteCredit achat2 = new CarteCredit(400, "Pantalon", 123456);
+        CarteCredit achat3 = new CarteCredit(500, "Installation wifi", 123457);
+
+
+        achat1.AfficherDetails();
+        achat2.AfficherDetails();
+        achat3.AfficherDetails();
     }
 }
